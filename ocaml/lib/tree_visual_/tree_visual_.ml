4b18df3b3b94e506fef649dc0e0d29ec10a2253c
halfminami <129936969+halfminami@users.noreply.github.com>
2023-11-04 01:30:23 +0900

    add assert test

--- ocaml/lib/tree_visual_/tree_visual_.ml
+++ ocaml/lib/tree_visual_/tree_visual_.ml
@@ -1,4 +1,14 @@
-let%test "hello" =
+open Base
+open Stdlib
+
+let%test "stack" =
   let () = print_endline "hello inline test!" in
   let () = Tree_visual.main () in
-  Int.equal 1 0
+  Int.equal 1 1
+
+(* let%test_unit "testtt" = [%test_eq: int] 1 0 *)
+let%test_unit "rev" =
+  [%test_eq: int] 0 0;
+  [%test_eq: int] 1 1;
+  [%test_eq: int] 1 0;
+  [%test_eq: int list] (List.rev [ 3; 2; 1 ]) [ 3; 2; 1 ]
