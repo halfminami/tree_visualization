59ad6c8399cf4e4f0b3def9c57b5c6265ea7024a
halfminami <129936969+halfminami@users.noreply.github.com>
2023-11-25 11:40:58 +0900

    relative build url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
   test: {
     include: ['src/tests/*.ts'],
   },
